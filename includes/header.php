d5e9cd4324292b39983cadb2478647a50b57db04
Rebecca <rebecca.colombel@hotmail.fr>
2020-12-14 12:15:59 +0100

    update : titre

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@
 </head>
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/webix/index.php">WEBIX</a>
+        <a class="navbar-brand" href="/webix/index.php">O.F.D.</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
