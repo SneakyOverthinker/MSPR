d570aa6867514b2502000d5495a2ecbefae4e843
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-12-14 12:42:35 +0100

    suppression du dossier connexion

--- connexiondbh.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Connexion à la base de donnée dbh
-function connectDB()
-{
-    return new PDO('mysql:host=localhost;dbname=MSPR-php', 'root', 'root',
-        [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']
-    );
-}
