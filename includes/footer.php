1a85b79ad0acf42c61e22284a9cfec4a188c365a
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-12-14 12:32:51 +0100

    footer

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div class="text-center" style="
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    margin-top: 100px;
+    background-color: #f5f5f5;"><p class="mt-5 mb-3 text-muted">&copy; 2020 - outfits of the day.com</p></div>
