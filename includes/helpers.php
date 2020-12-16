800e1dd9a61c1a2041691b485e6e02d41d6d7441
lau44 <lauryn.vandieren@ecoles-wis.net>
2020-12-16 10:51:31 +0100

    connexion/inscrire

--- includes/helpers.php
+++ includes/helpers.php
@@ -3,7 +3,7 @@
 // Connexion à la base de donnée dbh
 function connectDB()
 {
-    return new PDO('mysql:host=localhost;dbname=MSPR-php', 'root', '',
+    return new PDO('mysql:host=localhost;dbname=MSPR-php', 'root', 'root',
         [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']
     );
 }
